9c908b3aa37a356dcfbc26c1255cbb15ab35bd96
Hunter Gerlach <hunter.gerlach@gmail.com>
2017-01-17 03:07:21 -0500

    added mongoose dependency/path

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var appRoutes = require('./routes/app');
 
 var app = express();
+mongoose.connect('172.31.0.2:27017/node-angular'); //mongod docker container
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

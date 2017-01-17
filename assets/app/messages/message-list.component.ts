aed7c789320b6fbde7c961a8ef127cb1e489de04
Hunter Gerlach <hunter.gerlach@gmail.com>
2017-01-17 17:18:25 -0500

    implementing ngOnInit

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Message } from "./message.model";
+import { Message }        from "./message.model";
+import { MessageService } from "./message.service";
 
 @Component({
     selector: 'app-message-list',
@@ -12,13 +13,16 @@ import { Message } from "./message.model";
                 *ngFor="let message of messages">
             </app-message>
         </div>
-    `
+    `,
+    providers: [MessageService]
 })
 
-export class MessageListComponent {
-    messages: Message [] = [
-        new Message('Some message', 'Max'),
-        new Message('Some other message', 'Sam'),
-        new Message('Another message', 'Tom')
-    ];
+export class MessageListComponent implements OnInit {
+    messages: Message[];
+
+    constructor(private messageService: MessageService) {}
+
+    ngOnInit() {
+        this.messages = this.messageService.getMessages();
+    }
 }

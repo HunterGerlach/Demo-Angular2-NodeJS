d88f5efb28c90996827c2ab765374d9352aa3cdb
Hunter Gerlach <hunter.gerlach@gmail.com>
2017-01-18 00:30:50 -0500

    updated messages on frontend

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable }     from '@angular/core';
+import { Injectable, EventEmitter }     from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 
 import 'rxjs/Rx';
@@ -9,6 +9,7 @@ import { Message } from "./message.model";
 @Injectable()
 export class MessageService {
     private messages: Message[] = [];
+    messageIsEdit = new EventEmitter<Message>();
 
     constructor(private http: Http) {}
 
@@ -33,6 +34,14 @@ export class MessageService {
                 return transformedMessages;
             })
             .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+    editMessage(message: Message) {
+        this.messageIsEdit.emit(message);
+    }
+
+    updateMessage(message: Message) {
+        
     }
 
     deleteMessage(message: Message) {
